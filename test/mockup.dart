6ec356106af1a2f0079f3287d32172bc24ab0c75
Chuyen Trung Tran <chuyentt@gmail.com>
2022-12-03 23:06:51 +0700

    Version 1.3.1

--- test/mockup.dart
+++ test/mockup.dart
@@ -1,27 +1,27 @@
-import 'package:flutter_test/flutter_test.dart';
-// ignore: depend_on_referenced_packages
-import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
-// ignore: depend_on_referenced_packages
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// // ignore: depend_on_referenced_packages
+// import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+// // ignore: depend_on_referenced_packages
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-const String kTemporaryPath = 'temporaryPath';
-const String kApplicationSupportPath = 'applicationSupportPath';
-const String kDownloadsPath = 'downloadsPath';
-const String kLibraryPath = 'libraryPath';
-const String kApplicationDocumentsPath = 'applicationDocumentsPath';
-const String kExternalCachePath = 'externalCachePath';
-const String kExternalStoragePath = 'externalStoragePath';
+// const String kTemporaryPath = 'temporaryPath';
+// const String kApplicationSupportPath = 'applicationSupportPath';
+// const String kDownloadsPath = 'downloadsPath';
+// const String kLibraryPath = 'libraryPath';
+// const String kApplicationDocumentsPath = 'applicationDocumentsPath';
+// const String kExternalCachePath = 'externalCachePath';
+// const String kExternalStoragePath = 'externalStoragePath';
 
-class FakePathProviderPlatform extends Fake
-    with MockPlatformInterfaceMixin
-    implements PathProviderPlatform {
-  @override
-  Future<String?> getApplicationDocumentsPath() async {
-    return kApplicationDocumentsPath;
-  }
+// class FakePathProviderPlatform extends Fake
+//     with MockPlatformInterfaceMixin
+//     implements PathProviderPlatform {
+//   @override
+//   Future<String?> getApplicationDocumentsPath() async {
+//     return kApplicationDocumentsPath;
+//   }
 
-  @override
-  Future<String?> getApplicationSupportPath() async {
-    return kApplicationSupportPath;
-  }
-}
+//   @override
+//   Future<String?> getApplicationSupportPath() async {
+//     return kApplicationSupportPath;
+//   }
+// }
